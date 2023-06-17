702726bd6210e36e9e4c585633ffaaed50e27fb3
ChairmanAaron <aaronkwakye23@yahoo.com>
2023-06-17 03:26:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
 *main - A program that prints a line with puts function and not with printf
 *return = (0)
 */
-int main (void) {
+int main (void)
+{
 puts(“\"Programming is like building a multilingual puzzle”);
 return (0);
 }
+
