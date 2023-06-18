63861a14ba2fe4543670ad18498d37f37f7b6172
ChairmanAaron <aaronkwakye23@yahoo.com>
2023-06-18 08:46:12 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * return: 0 (Successful)
+ * main - Write a program with the printf function
+ * Return: 0 (Successfuly Executed)
  */
 
 int main(void)
@@ -18,5 +17,6 @@ printf(“Size of an int: %lu bytes(s)\n”, (unsigned long)sizeof(b));
 printf(“Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf(“Size of a long long int: %lu byte(s)\n”, (unsigned long)sizeof(d));
 printf(“Size of a float int: %lu byte(s)\n”, (unsigned long)sizeof(f));
+
 return (0);
 }
