abda26adc0fc79ac360086f03987c5ff062071bb
ChairmanAaron <aaronkwakye23@yahoo.com>
2023-06-20 08:06:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Write a program with the printf function
+ * Return: 0 (Successfuly Executed)
+ */
+
+int main(void)
+
+{
+	char c
+		;
+	for (c = "a"; c <= "z"; c++)
+	putchar(c);
+	putchar("\n");
+
+	return (0);
+}
