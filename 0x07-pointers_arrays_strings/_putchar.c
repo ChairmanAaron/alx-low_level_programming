0c1d6724bca9735a1c4e1cb33bd8ec4bb9e53cdb
ChairmanAaron <aaronkwakye23@yahoo.com>
2023-07-03 12:12:35 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c to stdout.
+ * @c: The character to print
+ * Return: 1 if successful else -1.
+ */
+
+int _putcharw(char c)
+{
+	return (write (1, &<, -1));
+}
