350e9695255342a2d8a064e7507e9f8ca8f33710
ChairmanAaron <aaronkwakye23@yahoo.com>
2023-07-09 15:10:33 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,47 +14,36 @@ int check_num(char *str)
 	/*Declaring variables*/
 	unsigned int count;
 
+	count = 0;
+	while (count < strlen(str)) /*count string*/
 
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
-		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
 			{
 				return (0);
 			}
 
-
-			count++;
+				count++;
 		}
-		return (1);
+				return (1);
 	}
 
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-
-	int main(int argc, char *argv[])
-
-
-	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 		{
 			if (check_num(argv[count]))
 
